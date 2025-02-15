3632fed8f45dd4a3a6d4d36712dee97fb5e02001
Dafa Armanto <randikadafa07@gmail.com>
2025-02-15 17:07:25 +0700

    Update Contact and Footer component and adding animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,40 @@
+import { FaDiscord, FaInstagram, FaTwitch, FaTwitter } from "react-icons/fa";
+
+const links = [
+  { href: "https://discord.com", icon: <FaDiscord /> },
+  { href: "https://instagram.com", icon: <FaInstagram /> },
+  { href: "https://x.com", icon: <FaTwitter /> },
+  { href: "https://twitch.com", icon: <FaTwitch /> },
+];
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="w-screen bg-violet-300 py-4 text-black">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm md:text-left">
+          &copy; Nova 2025. All Rights Reserved
+        </p>
+        <div className="flex justify-center gap-4 md:justify-start">
+          {links.map((link) => (
+            <a
+              href={link.href}
+              key={link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black transition-colors duration-500 ease-in-out hover:text-white"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
