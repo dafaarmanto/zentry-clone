fdec19329bb7a2696ffcc7c07f648f6bb68f0ece
Dafa Armanto <randikadafa07@gmail.com>
2025-02-16 22:29:20 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,12 @@ const Footer = () => {
     <footer className="w-screen bg-violet-300 py-4 text-black">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
         <p className="text-center text-sm md:text-left">
-          &copy; Nova 2025. All Rights Reserved
+          &copy; Nova 2025. All Rights Reserved.
         </p>
         <div className="flex justify-center gap-4 md:justify-start">
           {links.map((link) => (
             <a
-              href={link.href}
+              href={link}
               key={link}
               target="_blank"
               rel="noopener noreferrer"
